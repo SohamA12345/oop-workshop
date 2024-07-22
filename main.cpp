5054ecb50588448aee2bf21396a1f95458bc637c
Soham <sohamaich12345@gmail.com>
2024-07-22 13:02:07 +0930

    Adding test cases

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 extern int count(int[], int);
 
 int main() {
-    int array[5] = {4,5,6,7,8};
-    std::cout << "The number is: " << count(array, 5) << std::endl;
+    int array1[5] = {4,5,6,7,8};
+    std::cout << "The number is: " << count(array1, 5) << std::endl;
+    return 0;
+
+    int array2[7] = {2,2,2,2,2,0,2};
+    std::cout << "The number is: " << count(array2, 7) << std::endl;
+    return 0;
+
+    int array3[3] = {1,3,5};
+    std::cout << "The number is: " << count(array3, 5) << std::endl;
+    return 0;
+
+    int array4[3] = {0,0,0};
+    std::cout << "The number is: " << count(array4, 5) << std::endl;
     return 0;
 }
