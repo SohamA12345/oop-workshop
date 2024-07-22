2959646bed028c35ce53f99089d3b502a7679c84
Soham <sohamaich12345@gmail.com>
2024-07-22 13:10:51 +0930

    Fixed count.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     int array1[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array1, 5) << std::endl;
 
-    int array2[7] = {2,2,2,2,2,0,2};
+    /*int array2[7] = {2,2,2,2,2,0,2};
     std::cout << "The number is: " << count(array2, 7) << std::endl;
 
     int array3[3] = {1,3,5};
@@ -16,5 +16,6 @@ int main() {
 
     int array4[3] = {0,0,0};
     std::cout << "The number is: " << count(array4, 3) << std::endl;
+    */
     return 0;
 }
