709a897cdc29868e022ad975d9e5d7bb584b89cc
Soham <sohamaich12345@gmail.com>
2024-07-22 13:09:25 +0930

    Fixed count.cpp

--- count.cpp
+++ count.cpp
@@ -1,7 +1,7 @@
 int count(int array[], int n) {
   int j = 0;
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < n; i++) {
     if (array[i] % 2 == 0) {
       j++;
     }
--- main.cpp
+++ main.cpp
@@ -7,17 +7,14 @@ extern int count(int[], int);
 int main() {
     int array1[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array1, 5) << std::endl;
-    return 0;
 
     int array2[7] = {2,2,2,2,2,0,2};
     std::cout << "The number is: " << count(array2, 7) << std::endl;
-    return 0;
 
     int array3[3] = {1,3,5};
-    std::cout << "The number is: " << count(array3, 5) << std::endl;
-    return 0;
+    std::cout << "The number is: " << count(array3, 3) << std::endl;
 
     int array4[3] = {0,0,0};
-    std::cout << "The number is: " << count(array4, 5) << std::endl;
+    std::cout << "The number is: " << count(array4, 3) << std::endl;
     return 0;
 }
