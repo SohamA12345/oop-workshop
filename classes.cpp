91abc03d942c8cd68c39d85aa60f0d9545155958
Soham <sohamaich12345@gmail.com>
2024-08-08 22:31:32 +0930

    boo

--- classes.cpp
+++ classes.cpp
@@ -28,12 +28,22 @@ class Student {
     void set_name(string new_name) {name = new_name;}
     // void set_ID (int new_ID) {ID = new_ID;}
     void set_grade(double new_grade) {grade = new_grade;}
+
+    bool is_pass();
 };
 
 Student::Student(string s_name, int s_ID, double s_grade) {
   name = s_name;
   ID = s_ID;
   grade = s_grade;
+}
+
+bool Student::is_pass() {
+  if (grade < 50) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 int main() {
@@ -43,6 +53,8 @@ int main() {
   s1.set_grade(90.66);
   
   cout << s1.get_grade() << " " << s1.get_ID() << " " << s1.get_name() << endl;
+
+  cout << s1.is_pass() << endl;
 
   return 0;
 }
