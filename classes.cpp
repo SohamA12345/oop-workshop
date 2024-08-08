d673ee840eb35055290f6bde9c1a340794c14e5a
Soham <sohamaich12345@gmail.com>
2024-08-08 21:44:47 +0930

    boo

---
+++ classes.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+  private: // Cannot be accessed by other parts of the code.
+    string name; // Members variables or states
+    int ID;
+    double grade;
+
+  public:
+    Student(string s_name, int s_ID, double s_grade) { // Construct function initialises the private variables.
+      name = s_name;
+      ID = s_ID;
+      grade = s_grade;
+    }
+
+    // Behavior
+    string get_name() {return name;} // Member functions
+    int get_ID() {return ID;}
+    double get_grade() {return grade;}
+  
+    void set_name(string new_name) {name = new_name;}
+    // void set_ID (int new_ID) {ID = new_ID;}
+    void set_grade(double new_grade) {grade = new_grade;}
+};
+
+int main() {
+  Student s1("Soham", 1922802, 100.00); // Construct must be there while instantiating a class.
+
+  s1.set_name("Ham");
+  s1.set_grade(90.66);
+  
+  cout << s1.get_grade() << " " << s1.get_ID() << " " << s1.get_name() << endl;
+
+  return 0;
+}
