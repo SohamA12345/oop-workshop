9e5606b6018484fe339bc6c292866fa8cc747b3d
Soham <sohamaich12345@gmail.com>
2024-08-09 00:16:03 +0930

    Week 4 content done

--- Student.h
+++ Student.h
@@ -12,9 +12,12 @@ class Student {
 
   public:
     // Construct function initialises the private variables. Best to separate definition from declaration.
+    // Must match the name of header file.
     Student();
     Student(string s_name, double s_grade);
     Student(string s_name, int s_ID, double s_grade);
+
+    ~Student();
 
     // Or
     //Student(string s_name, int s_ID, double s_grade) { 
@@ -29,25 +32,27 @@ class Student {
     double get_grade() {return grade;}
   
     void set_name(string new_name) {name = new_name;}
-    // void set_ID (int new_ID) {ID = new_ID;}
+    void set_ID (int new_ID) {ID = new_ID;}
     void set_grade(double new_grade) {grade = new_grade;}
 
     bool is_pass(); // Best to separate member functions if even slightly complex.
 };
 
-Student::Student() {
-
+Student::Student(): name(""), ID(1), grade(0.0) { // Initialises private member variables // Must have a default constructer.
+  cout << "An  object of class Student was created\n";
 }
 
-Student::Student(string s_name, double s_grade) {
-  name = s_name;
-  grade = s_grade;
+Student::Student(string s_name, double s_grade): name(s_name), grade(s_grade), ID(0) { // Better way than later method.
 }
 
-Student::Student(string s_name, int s_ID, double s_grade) { // Must match the name of header file.
+Student::Student(string s_name, int s_ID, double s_grade) { 
   name = s_name;
   ID = s_ID;
   grade = s_grade;
+}
+
+Student::~Student() { // Automatically called to delete object once out of scope or while deleting a pointer, class dynamic array.
+  cout << "A student object was deleted\n";
 }
 
 bool Student::is_pass() {
