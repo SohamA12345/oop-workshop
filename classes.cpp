6d16eeb551894356a4373ff06093ae5fc28be95e
Soham <sohamaich12345@gmail.com>
2024-08-08 22:28:17 +0930

    boo

--- classes.cpp
+++ classes.cpp
@@ -10,11 +10,15 @@ class Student {
     double grade;
 
   public:
-    Student(string s_name, int s_ID, double s_grade) { // Construct function initialises the private variables.
-      name = s_name;
-      ID = s_ID;
-      grade = s_grade;
-    }
+    // Construct function initialises the private variables.
+    Student(string s_name, int s_ID, double s_grade);
+
+    // Or
+    //Student(string s_name, int s_ID, double s_grade) { 
+      //name = s_name;
+      //ID = s_ID;
+      //grade = s_grade;
+    //}
 
     // Behavior
     string get_name() {return name;} // Member functions
@@ -25,6 +29,12 @@ class Student {
     // void set_ID (int new_ID) {ID = new_ID;}
     void set_grade(double new_grade) {grade = new_grade;}
 };
+
+Student::Student(string s_name, int s_ID, double s_grade) {
+  name = s_name;
+  ID = s_ID;
+  grade = s_grade;
+}
 
 int main() {
   Student s1("Soham", 1922802, 100.00); // Construct must be there while instantiating a class.
