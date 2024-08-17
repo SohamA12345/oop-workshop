30471ac2a579eb9d92e469076fd6daa4260d2197
Soham <sohamaich12345@gmail.com>
2024-08-17 19:23:11 +0930

    Finished

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream> //<> bracket include file in the "include" folder
 #include "Justtrying.h"
-#include "temp/server.cpp" //"" includes a file from given path when compiled together
-
 
 using namespace std;
 
 int main() {
-  Server test;
-  string pasword;
+  // Server test;
+  // string pasword;
 
-  cin >> pasword;
+  // cin >> pasword;
 
-  test.login(pasword);
-  test.printLoginStatus();
+  // test.login(pasword);
+  // test.printLoginStatus();
   Justtrying();
 }
--- temp/server.cpp
+++ temp/server.cpp
@@ -1,37 +1,43 @@
 #include "Justtrying.h"
-
-#ifndef SERVER_DECLARATION
-#define SERVER_DECLARATION
-
-#include <string>
 #include <iostream>
 
-using namespace std;
+int Justtrying() { // Declaration of a function
+  cout << "Called a function" << endl;
+  return 0;
+}
 
-class Server
-{
-  private:
-    bool isLoggedIn = false;
-    string password = "password123";
-  public:
-    void login(string password) {
-      cout << this->password << '\t' << password << endl;
-      if(this->password == password) { // this can be used to access private variables
-        this->isLoggedIn = true;
-      }
-    }
+// #ifndef SERVER_DECLARATION
+// #define SERVER_DECLARATION
 
-    void printLoginStatus() {
-      if(this->isLoggedIn) {
-        cout << "I am logged in" << endl;
-      } else {
-        cout << "Try another password" << endl;
-      }
-    }
+// #include <string>
+// #include <iostream>
 
-    // Server();
-    // ~Server();
-};
+// using namespace std;
+
+// class Server
+// {
+//   private:
+//     bool isLoggedIn = false;
+//     string password = "password123";
+//   public:
+//     void login(string password) {
+//       cout << this->password << '\t' << password << endl;
+//       if(this->password == password) { // this can be used to access private variables
+//         this->isLoggedIn = true;
+//       }
+//     }
+
+//     void printLoginStatus() {
+//       if(this->isLoggedIn) {
+//         cout << "I am logged in" << endl;
+//       } else {
+//         cout << "Try another password" << endl;
+//       }
+//     }
+
+//     Server();
+//     ~Server();
+// };
 
 // Server::Server() {
   
@@ -41,10 +47,4 @@ class Server
 
 // }
 
-#endif
-
-int Justtrying() { // Declaration of a function
-  cout << "Called a function" << endl;
-  return 0;
-
-}
+// #endif
