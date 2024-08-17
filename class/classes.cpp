09275af1f16cb89d06580d75be0a5966911c4662
Soham <sohamaich12345@gmail.com>
2024-08-17 21:20:36 +0930

    Finished

--- class/classes.cpp
+++ class/classes.cpp
@@ -11,8 +11,12 @@ class Student {
     double grade;
 
   public:
+    int* test_array;
+    int size;
+
     // Construct function initialises the private variables. Best to separate definition from declaration.
     Student(string s_name, int s_ID, double s_grade);
+    Student(int size);
 
     // Or
     //Student(string s_name, int s_ID, double s_grade) { 
@@ -31,6 +35,14 @@ class Student {
     void set_grade(double new_grade) {grade = new_grade;}
 
     bool is_pass(); // Best to separate member functions if even slightly complex.
+
+    int* begin() { // Begining of the array
+      return test_array;
+    }
+
+    int* end() { // End of the array
+      return test_array+size;
+    }
 };
 
 Student::Student(string s_name, int s_ID, double s_grade) : name(s_name), ID(s_ID), grade(s_grade) {}
@@ -41,6 +53,10 @@ Student::Student(string s_name, int s_ID, double s_grade) {
   grade = s_grade;
 }
 */
+
+Student::Student(int size) : size(size) { // Creates an array in heap.
+  test_array = new int[size];
+}
 
 bool Student::is_pass() {
   if (grade < 50) {
@@ -57,9 +73,22 @@ int main() {
   s1.set_name("Ham");
   s1.set_grade(90.66);
   
-  cout << s1.get_grade() << " " << s1.get_ID() << " " << s1.get_name() << endl;
+  cout << s1.get_grade() << " " << s1.get_ID() << " " << s1.get_name() << endl; // 90.66 1922802 Ham
 
-  cout << s1.is_pass() << endl;
+  cout << s1.is_pass() << endl; // 1
+
+  // Using range
+  Student s2(3);
+
+  for (int i : s2) { // Accessing range of s2.
+    i += 1;
+    s2.test_array[i] = i;
+    cout << s2.test_array[i] << '\t';
+  }
+
+  cout << endl;
+
+  delete [] s2.test_array;
 
   return 0;
 }
