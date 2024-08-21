6fd62e53184aa6cc59592728319611b86982991b
Soham <sohamaich12345@gmail.com>
2024-08-21 20:58:25 +0930

    Finished question

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int hello(int x) {
-  cout << "Hi" << endl;
+class IdGenerator {
+  private:
+    int id;
+  public:
+    IdGenerator(int initialId) {this->id = initialId;}
+    IdGenerator(string initialId) {this->id = stoi(initialId);}
+    IdGenerator() : IdGenerator(10) {}
+    void setId(int id) {id = id;}
+    int getId() {return id;}
+    ~IdGenerator() {}
+};
+
+int main() {
+  IdGenerator* id = new IdGenerator[3] {0, 2, {"123"}}; // Must be same type to initialise the constructor or put additional braces.
+  for (int i = 0; i < 3; i++) {
+    cout << id[i].getId() << endl;
+  }
+  delete[] id;
   return 0;
 }
