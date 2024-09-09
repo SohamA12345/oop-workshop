89dd4578ee6c1e3c3b88c65e1fcd1ec98d0ea62f
Soham <sohamaich12345@gmail.com>
2024-09-09 13:20:21 +0930

    asdf

---
+++ University.h
@@ -0,0 +1,9 @@
+#include <string>
+
+class University {
+      private: 
+         std::string name;
+         std::string location;
+      public:
+         void addCourse(int id, std::string name);
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "University.h"
+#include "Course.h"
+#include "Grade.h"
+#include "Instructor.h"
+#include "Person.h"
+#include "Student.h"
+
+int main() {
+  return 0;
+}
