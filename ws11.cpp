ef9b6fe583f09153cfe7624d58087dcab8aa2328
Soham <sohamaich12345@gmail.com>
2024-10-16 17:22:13 +1030

    Done

--- ws11.cpp
+++ ws11.cpp
@@ -15,27 +15,53 @@ class RecordsManager {
 
  public:
   RecordsManager(string filename) : _filename(filename) {}
-  void read(Records &records) {
+  void read(Records& records) {
     _file.open(_filename, ios::in);
     if (_file.is_open()) {
       string line;
       while (std::getline(_file, line)) {
+        for (size_t i = 0; i < line.size(); i++) {
+          if ((line[i] < 'z' && line[i] > 'a') ||
+              (line[i] < 'Z' && line[i] > 'A')) {
+            _file.close();
+            throw runtime_error("Alphabents!");
+          }
+        }
+
+        int sum = 0;
+        for (size_t i = 0; i < line.size(); i++) {
+          sum += (line[i] - '0');
+        }
+
+        if (sum > 47) {
+          _file.close();
+          throw range_error("Out of range");
+        }
+
         records.push_back(stoi(line));
       }
       _file.close();
     }
   }
 };
+
 int main() {
-  RecordsManager receordM("test_clean.txt");
-  // RecordsManager receordM("test_corrupt1.txt");
+  // RecordsManager receordM("test_clean.txt");
+  RecordsManager receordM("test_corrupt1.txt");
   // RecordsManager receordM("test_corrupt2.txt");
+
   Records myRecords;
-  // reads records
-  receordM.read(myRecords);
+
+  try {
+    // reads records
+    receordM.read(myRecords);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+
   // calculate and print out the sum
   int sum = 0;
-  for (int i = 0; i < myRecords.size(); i++) {
+  for (size_t i = 0; i < myRecords.size(); i++) {
     sum += myRecords[i];
   }
   cout << sum << endl;
