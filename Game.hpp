eb684bf00f26d828eece4406dfa35dca810301be
Soham <sohamaich12345@gmail.com>
2024-10-24 16:41:34 +1030

    d

--- Game.hpp
+++ Game.hpp
@@ -2,6 +2,7 @@
 #define GRID_ITEM_HPP_6
 
 #include <vector>
+#include <iostream>
 #include "GridItem.hpp"
 #include "Robot.hpp"
 #include "Obstacle.hpp"
@@ -27,6 +28,29 @@ public:
       Obstacle(obstacleCoordinates[i].first, obstacleCoordinates[i].second, width, height);
     }
     GameState::PLAYING;
+  }
+  bool displayState() {
+   if (GameState::WIN) {
+    cout << "You win!";
+    return true;
+   } else if (GameState::LOSE) {
+    cout << "You lose :(";
+    return true;
+   } else if (GameState::PLAYING) {
+    cout << "Game on";
+    return false;
+   }
+  }
+  void movePlayer(int dx, int dy) {
+
+  }
+  void printGrid() {
+    for (int j = 0; j < height; j++) {
+      for (int i = 0; i < width; i++) {
+        cout << "_";
+      }
+      cout << "\n";
+    }
   }
   Game(/* args */);
   ~Game();
