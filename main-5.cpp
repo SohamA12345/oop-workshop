d574b9380c3ac40afd0378c518bfbcc5327ffe53
Soham <sohamaich12345@gmail.com>
2024-11-05 14:38:11 +1030

    done

---
+++ main-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+char* getArray () {
+  char newArray[4];
+  newArray[0] = 'b';
+  newArray[1] = 'e';
+  newArray[2] = 'n';
+  newArray[3] = 'y';
+  return newArray;
+}
+
+int main() {
+  char* newArray = getArray();
+  newArray[3] = 'y';
+  cout << string(newArray, 4) << endl;
+  delete newArray; // Seg Fault
+  return 0;
+}
